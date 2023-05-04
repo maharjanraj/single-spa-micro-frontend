f2086a7dacc128f32b385c3d8fb25bfb65f255f6
Raj Krishna Maharjan <rajmaharjan33@gmail.com>
2023-05-04 17:14:47 +0545

    added react, react-dom dependencies as in browser module

--- cra/webpack.config.js
+++ cra/webpack.config.js
@@ -10,15 +10,6 @@ module.exports = (webpackConfigEnv, argv) => {
   });
 
   return merge(defaultConfig, {
-    devServer: {
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods':
-          'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-        'Access-Control-Allow-Headers':
-          'X-Requested-With, content-type, Authorization',
-      },
-    },
     // modify the webpack config however you'd like to by adding to this object
   });
 };
